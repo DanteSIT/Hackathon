07a9331aa96722a0fe59709fd95b4c6207ec0f3a
Dante Lespoir <dantelespoir@gmail.com>
2025-05-22 15:55:00 +0400

    space

--- script.js
+++ script.js
@@ -74,6 +74,9 @@ $("#singup").click(function () { // Matched the ID exactly from your HTML
     $("#auth-message").text("Attempting to create user...");
     handleSignUp(email, password); // Call the signup function\]
 
+
+
+    
 });
 
 $("#login").click(function () {
